8b74adb450befad2366fd6eb4450fc30bc2167dd
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-06-13 21:00:38 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 ipes=$(curl -sS ipv4.icanhazip.com)
-surat=$(curl -sS https://raw.githubusercontent.com/kuhing/ip/vps | grep -w $ipes | awk '{print $4}')
+surat=$(curl -sS https://raw.githubusercontent.com/kuhing/ip/main/vps | grep -w $ipes | awk '{print $4}')
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 if [[ "$surat" = "true" ]]; then
   echo -ne
